69ff09afb1ad3fd76f3ab4e75f08a54830457ce4
rakibhasan197 <alhasanmdrakib062@gmail.com>
2026-04-29 20:33:00 +0600

    added something

---
+++ src/lib/auth.js
@@ -0,0 +1,18 @@
+import dns from "node:dns";
+dns.setServers(['8.8.8.8', '1.1.1.1'])
+import { betterAuth } from "better-auth";
+import { MongoClient } from "mongodb";
+import { mongodbAdapter } from "better-auth/adapters/mongodb";
+
+const client = new MongoClient(process.env.MONGODB_URI);
+const db = client.db("pixgen");
+
+export const auth = betterAuth({
+  database: mongodbAdapter(db, {
+
+    client
+  }),
+  emailAndPassword: { 
+    enabled: true, 
+  }, 
+});
